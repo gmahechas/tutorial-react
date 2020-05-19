a63c1a731d19bdb480910196a64f8e1b386e462a
gmahechas <gmahechas@ingenieros.com>
2020-05-18 20:27:17 -0500

    finish 24 burger builder

---
+++ 24-burger-app/src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css'
+
+const Layout = (props) => {
+  return (
+    <Aux>
+      <div>
+        Toolbar, SideDrawer, Backrop
+      </div>
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </Aux>
+  );
+}
+
+
+export default Layout;

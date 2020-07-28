fdfb11627af27dc0fc26dfc3f6a6fc3b63469825
gmahechas <gmahechas@ingenieros.com>
2020-07-28 14:36:31 -0500

    finish

--- 34-redux/src/App.tsx
+++ 34-redux/src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import { Router, RouteComponentProps, Link } from '@reach/router'
 
 import { SerieStore } from './serie/store/SerieStore';
 import Serie from './serie/Serie';
+import Home from './home/Home';
+import Fav from './serie/Fav';
+
+const RouterPage = (props: { component: JSX.Element } & RouteComponentProps) => props.component;
 
 function App() {
+
   return (
     <div>
-     <SerieStore>
-       <Serie />
-     </SerieStore>
+      <Link to="/">Home</Link> |{" "}
+      <Link to="series">Series</Link> |{" "}
+      <Link to="faves">Favourite(s)</Link>
+      <SerieStore>
+        <Router>
+          <RouterPage component={<Home />} path='/' />
+          <RouterPage component={<Serie />} path='series' />
+          <RouterPage component={<Fav />} path='faves' />
+        </Router>
+      </SerieStore>
     </div>
   )
 }

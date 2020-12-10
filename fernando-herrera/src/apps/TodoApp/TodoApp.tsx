4f510cef086cd016ff77d835f265ac5118b0edf3
gmahechas <gmahechas@ingenieros.com>
2020-12-10 18:31:09 -0500

    no start redux

--- fernando-herrera/src/apps/TodoApp/TodoApp.tsx
+++ fernando-herrera/src/apps/TodoApp/TodoApp.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useReducer } from 'react';
+import { todoReducer } from './todoReducer';
 
-const TodoApp = () => {
-  return (
-    <div>
-      <h1>Todo App</h1>
-      <hr />
-    </div>
-  )
+const initialState = [
+  { id: new Date().getTime(), desc: 'Aprendiendo React', done: false },
+];
+
+function init(initialState: any) {
+  return initialState;
 }
 
-export default TodoApp
+const TodoApp = () => {
+  const [todos] = useReducer(todoReducer, initialState, init);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h1>Todo App ({todos.length})</h1>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='description'></input>
+        <button type='submit'>Agregar</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((todo, i) => (
+          <li key={todo.id}>
+            {i + 1}. {todo.desc} <button>Borrar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;

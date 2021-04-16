c05468d4a122601546b9dfc3b46f2dc59641eccc
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-04-15 21:25:39 -0500

    update

--- fernando-herrera/src/index.tsx
+++ fernando-herrera/src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* import CounterApp from './apps/counter-app/CounterApp'; */
+import CounterApp from './apps/counter-app/CounterApp';
 /* import SimpleForm from './apps/simple-form/SimpleForm'; */
 /* import SimpleForm2 from './apps/simple-form/SimpleForm2'; */
 /* import FocusScreen from './apps/useRefApp/FocusScreen'; */
 /* import Memorize from './apps/memo/Memorize'; */
 /* import UseMemoApp from './apps/useMemo/UseMemoApp'; */
 /* import CallBall from './apps/memo/callback/CallBall'; */
-import TodoApp from './apps/TodoApp/TodoApp';
+/* import TodoApp from './apps/TodoApp/TodoApp'; */
 
 ReactDOM.render(
-  <React.StrictMode>
-    <TodoApp />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <CounterApp />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
